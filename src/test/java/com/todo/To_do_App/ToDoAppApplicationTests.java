7c5f19401e420c8937aa63ea7e75de8bc3f926b8
Nishmitha <nishmithas439@gmail.com>
2026-06-13 22:57:52 +0530

    Initial commit

---
+++ src/test/java/com/todo/To_do_App/ToDoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo.To_do_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
